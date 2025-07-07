58597361c6781ed1084ea22d8a0bdfe903856341
ArmandooAr <armando.dlvc@gmail.com>
2025-07-07 00:36:55 -0700

    practicando cosas basicas

--- aprendo.js
+++ aprendo.js
@@ -1,27 +1,16 @@
-//Implement a function named canExecuteFastAttack that takes a boolean value which indicates if the knight is awake. This function returns true if the 'Fast Attack' action is available based on the state of the character. Otherwise, returns false:
-const knightIsAwake = true;
-export function canExecuteFastAttack(knightIsAwake) {
-  return !knightIsAwake;
-}
+//Pide al usuario su nombre.
+//🔹 Pide su año de nacimiento.
+let anodenacimiento = prompt("¿Cuál es tu año de nacimiento?");
+//🔹 Guarda el año actual (por ejemplo, 2025).
+let añoactual = 2025;
+//🔹 Calcula su edad restando año actual menos año de nacimiento.
+let edad = añoactual - Number(anodenacimiento);
+//🔹 Determina si es mayor de edad (18 años o más).
+let mayordeedad = edad >= 18;
+//🔹 Crea un mensaje que diga su nombre
+let nombre = prompt("¿Cuál es tu nombre?");
+//  su edad y si es mayor de edad o no.
+let mayor = 
+//🔹 Muestra ese mensaje con console.log y con document.write. 
 
-//Implement a function named canSpy that takes three boolean values, indicating if the knight, archer and the prisoner, respectively, are awake. The function returns true if the 'Spy' action is available based on the state of the characters. Otherwise, returns false:
-const archerIsAwake = true;
-const prisonerIsAwake = false;
-export function canSpy(knightIsAwake, archerIsAwake, prisonerIsAwake) {
-  return knightIsAwake || archerIsAwake || prisonerIsAwake;
-}
 
-//Implement a function named canSignalPrisoner that takes two boolean values, indicating if the archer and the prisoner, respectively, are awake. The function returns true if the 'Signal Prisoner' action is available based on the state of the characters. Otherwise, returns false:
-export function canSignalPrisoner(archerIsAwake, prisonerIsAwake) {
-  return (!archerIsAwake && prisonerIsAwake);
-}
-//Implement a function named canFreePrisoner that takes four boolean values. The first three parameters indicate if the knight, archer and the prisoner, respectively, are awake. The last parameter indicates if Annalyn's pet dog is present. The function returns true if the 'Free Prisoner' action is available based on the state of the characters and Annalyn's pet dog presence. Otherwise, it returns false:
-const petDogIsPresent =false;
-export function canFreePrisoner(knightIsAwake, archerIsAwake, prisonerIsAwake, petDogIsPresent) {
-  return (
-    (petDogIsPresent && !archerIsAwake) ||
-    (!knightIsAwake && !archerIsAwake && prisonerIsAwake)
-  );
-}   
-
-console.log(canFreePrisoner(knightIsAwake, archerIsAwake, prisonerIsAwake, petDogIsPresent));
--- dos.js
+++ dos.js
@@ -5,3 +5,8 @@ export function frontDoorResponse(line) {
 // Ejemplo de uso:
 console.log(frontDoorResponse('Stands so high'));    // Output: 'S'
 console.log(frontDoorResponse('Huge hooves too'));   // Output: 'H'
+
+export function frontDoorPassword (word) {
+  return word;
+}
+console.log(frontDoorPassword('word')); // Output: 'password'
