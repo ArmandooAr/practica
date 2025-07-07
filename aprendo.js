24eb915f0c2c17a525f709313716fd48fadb3a70
ArmandooAr <armando.dlvc@gmail.com>
2025-07-07 10:58:36 -0700

    practicando cosas basicas

--- aprendo.js
+++ aprendo.js
@@ -6,11 +6,19 @@ let añoactual = 2025;
 //🔹 Calcula su edad restando año actual menos año de nacimiento.
 let edad = añoactual - Number(anodenacimiento);
 //🔹 Determina si es mayor de edad (18 años o más).
-let mayordeedad = edad >= 18;
+let mayordeedad = MayoriaDeEdad(edad);
 //🔹 Crea un mensaje que diga su nombre
 let nombre = prompt("¿Cuál es tu nombre?");
 //  su edad y si es mayor de edad o no.
-let mayor = 
-//🔹 Muestra ese mensaje con console.log y con document.write. 
+function MayoriaDeEdad ( mayordeedad) {
+        if (mayordeedad >= 18) {
+        return true;
+        }else {
+        return false;
+    }   
+ }
+//🔹 Muestra ese mensaje con console.log y con document.write.
+console.log(`Hola ${nombre}, tienes ${edad} años y eres ${mayordeedad ? "mayor de edad" : "menor de edad"}.`);
+document.write(`Hola ${nombre}, tienes ${edad} años y eres ${mayordeedad ? "mayor de edad" : "menor de edad"}.`); 
 
 
